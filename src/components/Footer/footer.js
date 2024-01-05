0dc00cc35a185582dbcdfe9eb3db20a27e9e0624
arthureastwood <arthurpenragon@gmail.com>
2024-01-05 14:35:54 -0800

    Added Social Media Links

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import './footer.css';
-import inst from '../../assets/instagram.png';
-import whats from '../../assets/whatsapp.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     console.log(new Date().getFullYear());
   return (
     <footer className='footer'>
         <div className='social-icons'>
-            <a href='https://instagram.com/nurture.nfpo?igshid=MzNINGNkZWQ4Mg==' target='_blank' rel="noopener noreferrer" className='instaMag'><img src={inst} alt='Instagram Icon' /> </a>
-            <a href='https://wa.me/+256787750506' target='_blank' rel="noopener noreferrer" className='whatsMag'><img src={whats} alt='Whatsapp Icon' /> </a>
+            <a href="https://instagram.com/nurture.nfpo?igshid=MzNINGNkZWQ4Mg==" className="instagram social" target='_blank' rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faInstagram} size="2x" />
+            </a>
+            <a href="https://wa.me/+256787750506" className="whatsapp social" target='_blank' rel="noopener noreferrer">
+              <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+            </a>
         </div>
         <div className='year'>
             &copy;Nurture Organization, {new Date().getFullYear()}. All Rights Reserved.
